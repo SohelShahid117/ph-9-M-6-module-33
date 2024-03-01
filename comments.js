800344831a656dfb6a997e00c9d7742516adb866
SohelShahid117 <sohelshahid09@gmail.com>
2024-03-01 23:03:50 +0600

    complete the module-33

--- comments.js
+++ comments.js
@@ -14,7 +14,6 @@ const loadComments2 = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/comments')
     const data = await res.json()
     console.log(data)
-
 }
 
 const loadComments3 = async () => {
@@ -26,6 +25,4 @@ const loadComments3 = async () => {
     } catch (err) {
         console.log(err)
     }
-
-
 }
